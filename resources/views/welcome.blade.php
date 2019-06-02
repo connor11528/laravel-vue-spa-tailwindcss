1c542f39ae1c941133efe7f16d3abe48cfcba8b9
Connor Leech <connorleech@gmail.com>
2019-06-02 07:35:42 -0700

    add tailwind css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,53 +18,63 @@
         <link rel="stylesheet" href="https://unpkg.com/tailwindcss/dist/tailwind.min.css">
         <!--Replace with your tailwind.css once created-->
 
-
-
+        <style>
+            .gradient {
+                background: linear-gradient(90deg, #d53369 0%, #daae51 100%);
+            }
+        </style>
     </head>
     <body class="leading-normal tracking-normal text-white gradient" style="font-family: 'Source Sans Pro', sans-serif;">
-
-        <!--Nav-->
         <nav id="header" class="fixed w-full z-30 top-0 text-white">
-
             <div class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
-
                 <div class="pl-4 flex items-center">
                     <a class="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl"  href="#">
                         <!--Icon from: http://www.potlabicons.com/ -->
                         <svg class="h-8 fill-current inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512.005 512.005">
                             <rect fill="#2a2a31" x="16.539" y="425.626" width="479.767" height="50.502" transform="matrix(1,0,0,1,0,0)" fill="rgb(0,0,0)" />
                             <path class="plane-take-off" d=" M 510.7 189.151 C 505.271 168.95 484.565 156.956 464.365 162.385 L 330.156 198.367 L 155.924 35.878 L 107.19 49.008 L 211.729 230.183 L 86.232 263.767 L 36.614 224.754 L 0 234.603 L 45.957 314.27 L 65.274 347.727 L 105.802 336.869 L 240.011 300.886 L 349.726 271.469 L 483.935 235.486 C 504.134 230.057 516.129 209.352 510.7 189.151 Z "/>
-                        </svg> LANDING
+                        </svg> EMPLOYBL
                     </a>
                 </div>
-
                 <div class="block lg:hidden pr-4">
                     <button id="nav-toggle" class="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-gray-800 hover:border-teal-500 appearance-none focus:outline-none">
                         <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
                     </button>
                 </div>
-
                 <div class="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20" id="nav-content">
                     <ul class="list-reset lg:flex justify-end flex-1 items-center">
                         <li class="mr-3">
-                            <a class="inline-block py-2 px-4 text-black font-bold no-underline" href="#">Active</a>
+                            <a class="inline-block py-2 px-4 text-black font-bold no-underline" href="#">
+                                For employers
+                            </a>
                         </li>
                         <li class="mr-3">
-                            <a class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#">link</a>
+                            <a class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#">
+                                Job search
+                            </a>
                         </li>
                         <li class="mr-3">
-                            <a class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#">link</a>
+                            <a class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#">
+                                Blog
+                            </a>
+                        </li>
+                        <li class="mr-3">
+                            <a class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#">
+                                About
+                            </a>
                         </li>
                     </ul>
-                    <button id="navAction" class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75">Action</button>
+                    <button id="navAction" class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75">
+                        Sign up
+                    </button>
+                    <a class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#">
+                        Login
+                    </a>
                 </div>
             </div>
 
             <hr class="border-b border-gray-100 opacity-25 my-0 py-0" />
         </nav>
-
-
-
 
         <!--Hero-->
         <div class="pt-24">
@@ -72,18 +82,18 @@
             <div class="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
                 <!--Left Col-->
                 <div class="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
-                    <p class="uppercase tracking-loose w-full">What business are you?</p>
-                    <h1 class="my-4 text-5xl font-bold leading-tight">Main Hero Message to sell yourself!</h1>
-                    <p class="leading-normal text-2xl mb-8">Sub-hero message, not too long and not too short. Make it just right!</p>
+                    <p class="uppercase tracking-loose w-full">TALENT ACQUISITION</p>
+                    <h1 class="my-4 text-5xl font-bold leading-tight">Fu#@ hiring fees</h1>
+                    <p class="leading-normal text-2xl mb-8">Find quality active candidates without third party recruiters. Hire all the people.</p>
 
 
 
-                    <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg">Subscribe</button>
+                    <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg">Start hiring</button>
 
                 </div>
                 <!--Right Col-->
                 <div class="w-full md:w-3/5 py-6 text-center">
-                    <img class="w-full md:w-4/5 z-50" src="hero.png">
+                    <img class="w-full md:w-4/5 z-50" src="https://i.imgur.com/fjXbO4U.png">
                 </div>
 
             </div>
@@ -110,7 +120,7 @@
 
         <section class="bg-white border-b py-8">
             <div class="container max-w-5xl mx-auto m-8">
-                <h1 class="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">Title</h1>
+                <h1 class="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">Find candidates</h1>
                 <div class="w-full mb-4">
                     <div class="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
                 </div>
@@ -428,7 +438,7 @@
                             <svg class="h-8 fill-current inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512.005 512.005">
                                 <rect fill="#2a2a31" x="16.539" y="425.626" width="479.767" height="50.502" transform="matrix(1,0,0,1,0,0)" fill="rgb(0,0,0)" />
                                 <path class="plane-take-off" d=" M 510.7 189.151 C 505.271 168.95 484.565 156.956 464.365 162.385 L 330.156 198.367 L 155.924 35.878 L 107.19 49.008 L 211.729 230.183 L 86.232 263.767 L 36.614 224.754 L 0 234.603 L 45.957 314.27 L 65.274 347.727 L 105.802 336.869 L 240.011 300.886 L 349.726 271.469 L 483.935 235.486 C 504.134 230.057 516.129 209.352 510.7 189.151 Z "/>
-                            </svg> LANDING
+                            </svg> EMPLOYBL
 
                         </a>
                     </div>
